ebcd61df251228a32b47cbf586580e639f301d0b
amjadkhansarwar <muhammad.amjad@iths.se>
2023-03-30 09:59:26 +0200

    working on the api.js file

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,7 @@ import React from 'react'
 import axios from 'axios'
 const WEATHER_API_KEY='a03d5359fd3cca8ba8a852c9e86b749c'
 const TRAFFIC_API_KEY='1f43b48aa22945f587a7865064f9c2e2'
+const TRAFFIC_BASE_URL=`https://api.sl.se/api2/realtimedeparturesV4.json?key=${TRAFFIC_API_KEY}&siteid=9192&timewindow=40`
 
 export const GetWeather = async () => {
     try {
@@ -15,11 +16,12 @@ export const GetWeather = async () => {
 }
 
 export const GetTraffic = async () => {
-     axios.get('https://api.sl.se/api2/realtimedeparturesV4.json?key=1f43b48aa22945f587a7865064f9c2e2&siteid=9192&timewindow=40')
-    .then(response => {
-        console.log(response.data.ResponseData.Metros)
-    })
-    .catch(error => {
+    try {
+        const response = await fetch(`https://api.sl.se/api2/realtimedeparturesV4.json?key=1f43b48aa22945f587a7865064f9c2e2&siteid=9192&timewindow=40`)
+        const data = await response.json()
+        return data
+    }
+    catch (error) {
         console.log(error)
-    })
+    }
 }
